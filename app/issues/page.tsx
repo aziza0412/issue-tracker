f5aa924b7484239f0635daf2e706b5610a113e5f
aziza0412 <azizabenhalima@live.fr>
2024-02-06 20:43:23 +0100

    styling

---
+++ app/issues/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const IssuesPage = () => {
+   
+  return (
+    <div>
+      Issues pages
+    </div>
+  )
+}
+
+export default IssuesPage
